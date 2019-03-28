c8bd47528b0900ad888f86a04db5b1226c12256a
Timofey Utrobin <utrobin.timofey@yandex.ru>
2019-03-29 01:46:01 +0500

    renamed variable

--- read.py
+++ read.py
@@ -1,6 +1,6 @@
 def get_byte_stream(fileName):
-    with open(fileName, 'rb') as f:
-        byte = f.read(1)
+    with open(fileName, 'rb') as file:
+        byte = file.read(1)
         while byte:
             yield byte
-            byte = f.read(1)
+            byte = file.read(1)
